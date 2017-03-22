2de76bf7f1ed2debeb1957cc4a0561ba75472412
veeramarni <mvasireddy@cdmbase.com>
2017-03-22 18:43:14 -0400

    fixed redux fail issue

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import thunk from "redux-thunk";
 import { bootstrap } from "redux-bootstrap";
-import * as createLogger from "redux-logger";
+// import createLogger from "redux-logger";
+const createLogger = require('redux-logger')
 import { throttle } from "lodash";
 import routes from "./config/routes";
 import reposReducer from "./reducers/repos_reducer";
@@ -13,7 +14,7 @@ declare var __PRELOADED_STATE__: any;
 let middleware: any[] = [thunk];
 
 if (process.env.NODE_ENV !== "production") {
-    middleware.push(createLogger());
+    middleware.push(createLogger({level: 'log'}));
 }
 
 let preloadedState: any = null;
